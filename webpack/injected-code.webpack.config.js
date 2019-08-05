80b89037548a3fdacc12e722e414b61ae97c7775
Simon Siefke <simon.siefke@gmail.com>
2019-08-05 21:02:17 +0200

    fix: build shoulnt output commonjs for the browser

--- webpack/injected-code.webpack.config.js
+++ webpack/injected-code.webpack.config.js
@@ -4,11 +4,13 @@ const root = path.join(__dirname, '..');
 
 module.exports = withDefaults({
 	context: path.join(root, 'packages/injected-code'),
+	target: 'web', // injected code runs in the browser
 	entry: {
 		'injected-code': './src/injectedCodeMain.ts'
 	},
 	output: {
 		filename: 'injectedCodeMain.js',
-		path: path.join(root, 'dist/packages/injected-code/dist')
+		path: path.join(root, 'dist/packages/injected-code/dist'),
+		libraryTarget: 'var' // build for browser
 	}
 });
