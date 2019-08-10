6749ccfe7944701d4a2c0006521dd3191a8fed04
Simon Siefke <simon.siefke@gmail.com>
2019-08-10 17:45:04 +0200

    docs: add todo

--- tempdir.js
+++ tempdir.js
@@ -2,4 +2,5 @@ const os = require('os');
 
 os.tmpdir(); // ?
 
-eval('fs.readFileSync(path.join(__dirname, `process.js`))')//?
+eval('fs.readFileSync(path.join(__dirname, `process.js`))'); // ?
+// TODO use this for caching
