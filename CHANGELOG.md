26493850f2849ce4a09ad755cae979de249148eb
Simon Siefke <simon.siefke@gmail.com>
2019-08-31 09:27:16 +0200

    fix: remove url bar because its ugly

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 | Version | Date     | Changelog                                                                                                                                                                                           |
 | ------- | -------- | --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| 1.15.7  | 31.08.19 | &mdash; **_[Fix]_** remove url bar in vscode because its ugly                                                                                                                                       |
 | 1.15.6  | 31.08.19 | &mdash; **_[Fix]_** use csp when opening preview in vscode                                                                                                                                          |
 | 1.15.5  | 30.08.19 | &mdash; **_[Fix]_** support for html entities                                                                                                                                                       |
 | 1.15.2  | 27.08.19 | &mdash; **_[Fix]_** support for element moves                                                                                                                                                       |
--- packages/extension/src/open/open-in-vscode/openInVscodeHtml.ts
+++ packages/extension/src/open/open-in-vscode/openInVscodeHtml.ts
@@ -2,6 +2,7 @@ export const html = `<!DOCTYPE html>
 <html>
   <head>
     <meta charset="UTF-8" />
+    <!-- TODO safer csp -->
     <meta
       http-equiv="Content-Security-Policy"
       content="default-src 'none'; style-src 'unsafe-inline'; script-src 'unsafe-inline'; frame-src http://localhost:3000"
@@ -12,18 +13,24 @@ export const html = `<!DOCTYPE html>
         background-color: #3297df;
         color: white;
       }
+      html,body{
+        height:100%;
+      }
       body {
         margin: 0;
         padding: 0;
         background: white;
+        display: flex;
+        flex-direction: column
       }
       iframe {
-        height: 90vh;
-        width: 100%;
+        flex: 1;
       }
       form {
         display: flex;
-        border-bottom: 2px solid black;
+        border-bottom: 1px solid black;
+        padding: 0.2rem 0.4rem;
+        background: var(--vscode-editor-background)
       }
       input {
         all: unset;
@@ -31,11 +38,26 @@ export const html = `<!DOCTYPE html>
         padding: 0.3rem;
         font-family: sans-serif;
         color: rgb(39, 39, 39);
+        background: var(--vscode-input-background);
+        color: var(--vscode-input-foreground);
+        border: 1px solid currentColor;
+        box-sizing: border-box;
+        
+      }
+      input:focus{
+        outline: none;
+        border-color: var(--vscode-inputValidation-infoBorder);
+      }
+      form{
+        display: none
       }
     </style>
   </head>
   <body>
     <form>
+      <button id="forward" aria-label="forward">&lt;-</button>
+      <button id="backward" aria-label="backward">-&gt;</button>
+      <button id="refresh" aria-label="refresh">&#8635;</button>
       <input type="text" placeholder="Enter address" value="http://localhost:3000" />
     </form>
     <iframe src="http://localhost:3000" frameBorder="0"></iframe>
@@ -47,6 +69,13 @@ export const html = `<!DOCTYPE html>
         event.preventDefault()
         console.log($input.value)
         $iframe.src = $input.value
+      })
+
+      const $forward = document.getElementById('forward')
+      const $backward = document.getElementById('backward')
+      const $refresh = document.getElementById('refresh')
+      $refresh.addEventListener('click', ()=>{
+        
       })
     </script>
   </body>
