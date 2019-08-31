40b875bf4b2f17b0408b0aae3bfd36fc9fb9bf89
Simon Siefke <simon.siefke@gmail.com>
2019-08-31 07:29:43 +0200

    fix: add csp when opening preview in vscode

--- packages/extension/src/open/open-in-vscode/openInVscodeHtml.ts
+++ packages/extension/src/open/open-in-vscode/openInVscodeHtml.ts
@@ -1,10 +1,11 @@
 export const html = `<!DOCTYPE html>
-<html lang="en">
+<html>
   <head>
     <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Document</title>
+    <meta
+      http-equiv="Content-Security-Policy"
+      content="default-src 'none'; style-src 'unsafe-inline'; script-src 'unsafe-inline'; frame-src http://localhost:3000"
+    >
     <style>
       /* restore default selection colors */
       ::selection {
