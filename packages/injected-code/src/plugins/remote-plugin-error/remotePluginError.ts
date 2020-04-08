0d65af190879ffce30fa66ade0edf440406bc2ed
Simon Siefke <simon.siefke@gmail.com>
2020-04-08 21:27:36 +0200

    fix: less annoying display of errors

--- packages/injected-code/src/plugins/remote-plugin-error/remotePluginError.ts
+++ packages/injected-code/src/plugins/remote-plugin-error/remotePluginError.ts
@@ -2,6 +2,6 @@ import { RemotePlugin } from '../remotePluginApi'
 
 export const remotePluginError: RemotePlugin = api => {
   api.webSocket.onMessage('error', payload => {
-    alert('error' + payload.message)
+    console.error('error' + payload.message)
   })
 }
