3b940386f88035e677b61eb79a39c32ca950d7a4
Simon Siefke <simon.siefke@gmail.com>
2022-12-31 00:19:42 +0100

    remove unused code (#52)

--- packages/html-preview-service-node/src/htmlPreviewServiceNodeMain.ts
+++
@@ -1,7 +0,0 @@
-export {
-  createHttpServerNew as createHttpServer,
-  HttpServer,
-} from './HttpServer/createHttpServerNew'
-export * from './openInBrowser/openInBrowser'
-export * from './WebSocketServer/createWebSocketServer'
-export * from './url/url'
