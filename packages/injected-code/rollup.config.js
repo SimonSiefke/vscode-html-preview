88a9aa934183fcc102126a1decec1eb00d05cfb7
Simon Siefke <simon.siefke@gmail.com>
2022-12-30 17:02:31 +0100

    try to fix rollup config

--- packages/injected-code/rollup.config.js
+++ packages/injected-code/rollup.config.js
@@ -1,25 +1,26 @@
-import pluginTypescript from '@rollup/plugin-typescript';
-import terser from '@rollup/plugin-terser';
+import pluginTypescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
 // @ts-ignore
-import pkg from './package.json' assert {type:'json'}
+import pkg from './package.json' assert { type: 'json' }
 import typescript from 'typescript'
 
 export default {
-	input: 'src/injectedCodeMain.ts',
-	output: [
-		{
-			file: pkg.main,
-			format: 'es',
-			sourcemap: true
-		}
-	],
-	external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
-	plugins: [
-		pluginTypescript({
-			typescript
-		}),
-		terser({
-			mangle: false // keep output readable for debugging, even in production
-		})
-	]
-};
+  input: 'src/injectedCodeMain.ts',
+  output: [
+    {
+      file: pkg.main,
+      format: 'es',
+      sourcemap: true,
+    },
+  ],
+  external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
+  plugins: [
+    pluginTypescript({
+      typescript,
+      outputToFilesystem: false,
+    }),
+    terser({
+      mangle: false, // keep output readable for debugging, even in production
+    }),
+  ],
+}
